47c8335c182646ac26db862559bb3c48965f5009
mahdishub <mahdi.ahmed@welldev.io>
2025-01-25 04:30:01 -0400

    Implemented all state machine classes

---
+++ GGJ 2025/Assets/Scripts/MovingObjectScripts/MovingObjectBubbledState.cs
@@ -0,0 +1,20 @@
+namespace MovingObjectScripts
+{
+    public class MovingObjectBubbledState : MovingObjectBaseState
+    {
+        public override void EnterState(MovingObjectStateManager movingObjectStateManager)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void UpdateState(MovingObjectStateManager movingObjectStateManager)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ExitState(MovingObjectStateManager movingObjectStateManager)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
