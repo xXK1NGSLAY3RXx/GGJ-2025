70416e3ee8e5f924db17460ccb247b3fd6dd4086
MHBaghdadi <mh1377@gmail.com>
2025-01-25 02:39:21 -0400

    -Minor changes

--- GGJ 2025/Assets/Scripts/CameraFollower.cs
+++ GGJ 2025/Assets/Scripts/CameraFollower.cs
@@ -4,12 +4,14 @@ public class CameraFollower : MonoBehaviour
 {
     public Transform followTarget;
     public Vector3 offset;
+    public float smoothness;
 
     private void Update()
     {
+        offset.z += Input.mouseScrollDelta.y;
         if (!followTarget)
             return;
 
-        transform.position = Vector3.Lerp(transform.position, followTarget.position + offset, Time.deltaTime * 5);
+        transform.position = Vector3.Lerp(transform.position, followTarget.position + offset, Time.deltaTime * smoothness);
     }
 }
