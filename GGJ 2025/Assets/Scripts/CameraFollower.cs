6ca13ea5add7928fb5867df678a227baec28c280
MHBaghdadi <mh1377@gmail.com>
2025-01-24 21:50:56 -0400

    -Added Camera follow script

---
+++ GGJ 2025/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform followTarget;
+    public Vector3 offset;
+
+    private void Update()
+    {
+        if (!followTarget)
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, followTarget.position + offset, Time.deltaTime * 5);
+    }
+}
